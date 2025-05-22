412788c5a65dc321c9d112e26e449547cb06dab0
YuneidyC <yuneidycastillo25@gmail.com>
2025-05-22 18:46:34 +0100

    Add menu page with styles

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,96 @@
+import { desserts, drinks, menuTacos, others, quesadillas } from './menuList.js';
+
+const taco = './assets/images/tacoImg.png';
+const rice = './assets/images/riceImg.png';
+const cookie = './assets/images/cookieImg.png';
+const tequila = './assets/images/tequilaImg.png';
 const redChile = './assets/images/redChileImg.png';
 const greenChile = './assets/images/greenChileImg.png';
+
 const main = document.getElementsByClassName('main')[0];
 
 const menuContainer = document.createElement('div');
 menuContainer.classList.add('menu-container');
 
+const text = document.createElement('p');
+text.classList.add('description');
+menuContainer.appendChild(text);
+
+const txtDescription = document.createTextNode(' Asada, carnitas, al pastor, chicken, ground beef and chicarron.');
+text.appendChild(txtDescription);
 main.appendChild(menuContainer);
 
 createCardsMenu();
 createRedAndGreenPepper();
+
+function createTitleCardsMenu(parent, text, className) {
+    const title = document.createElement('p');
+    const cardTitle = document.createTextNode(text);
+    title.classList.add(className);
+    parent.appendChild(title);
+    title.appendChild(cardTitle);
+}
+
+function createImgMenu(parent, src, className) {
+    const img = document.createElement('img');
+    img.src = src;
+    img.classList.add(className);
+    parent.appendChild(img);
+}
 
 function createCardsMenu() {
     for (let i = 0; i < 3; i++) {
         const cardsMenu = document.createElement('div');
         cardsMenu.classList.add('card-menu-' + i);
         menuContainer.appendChild(cardsMenu);
+        if (i === 0) {
+            createTitleCardsMenu(cardsMenu, 'Tacos & Burritos', 'card-menu-title');
+            createImgMenu(cardsMenu, taco, 'taco');
+            createMenus(cardsMenu, 'taco', menuTacos);
+            createTitleCardsMenu(cardsMenu, 'Quesadillas', 'card-menu-title');
+            createMenus(cardsMenu, 'quesadillas', quesadillas);
+        } else if (i === 1) {
+            createTitleCardsMenu(cardsMenu, 'Others', 'card-menu-title');
+            createImgMenu(cardsMenu, rice, 'rice');
+            createMenus(cardsMenu, 'other', others);
+        } else {
+            createTitleCardsMenu(cardsMenu, 'Desserts', 'card-menu-title');
+            createImgMenu(cardsMenu, cookie, 'cookie');
+            createMenus(cardsMenu, 'desserts', desserts);
+            createTitleCardsMenu(cardsMenu, 'Drinks', 'card-menu-title');
+            createImgMenu(cardsMenu, tequila, 'tequila');
+            createMenus(cardsMenu, 'drinks', drinks);
+        }
+    }
+}
+
+function createMenus(parent, className, menu) {
+    const itemsList = document.createElement('ol');
+    parent.appendChild(itemsList);
+
+    for (let i = 0; i < menu.length; i++) {
+        const item = document.createElement('li');
+        itemsList.appendChild(item);
+        item.classList.add(className + '-' + i);
+
+        const mealName = document.createElement('span');
+        mealName.classList.add('meal');
+        item.appendChild(mealName);
+
+        const mealTextName = document.createTextNode(menu[i].meal);
+        mealName.appendChild(mealTextName);
+
+        const mealPrice = document.createElement('span');
+        mealPrice.classList.add('price');
+        item.appendChild(mealPrice);
+
+        const mealTextPrice = document.createTextNode('$' + menu[i].price);
+        mealPrice.appendChild(mealTextPrice);
     }
 }
 
 function createRedAndGreenPepper() {
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 11; i++) {
         const red = document.createElement('img');
         red.className = 'red-chile red-chile-' + i;
         red.src = redChile;
