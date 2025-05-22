2f083d078e254b7aaaccc8d074d89090ba740d25
YuneidyC <yuneidycastillo25@gmail.com>
2025-05-22 18:14:26 +0100

    Add menu list

---
+++ src/menuList.js
@@ -0,0 +1,51 @@
+const menuTacos = [
+    { meal: 'Birria', price: 10.00 },
+    { meal: 'Al Pastor', price: 12.00 },
+    { meal: 'Tinga', price: 10.00 },
+    { meal: 'Carnitas', price: 18.00},
+    { meal: 'Mixta', price: 10.00},
+    { meal: 'Fish', price: 15.00},
+    { meal: 'Chicken', price: 10.00},
+];
+
+const quesadillas = [
+    { meal: 'Mexicana', price: 10.00 },
+    { meal: 'Jalisco', price: 10.00 },
+    { meal: 'Ranchera', price: 17.00 },
+    { meal: 'Fajita', price: 10.00},
+    { meal: 'Special', price: 18.00},
+];
+
+const others = [
+    { meal: 'Cheese Nachos', price: 8.00 },
+    { meal: 'Bean & Cheese Nachos', price: 10.00 },
+    { meal: 'Tortilla Soup', price: 12.00 },
+    { meal: 'Chicken Soup', price: 10.00},
+    { meal: 'Tamales', price: 9.00},
+    { meal: 'Chile Rellenos', price: 10.00},
+    { meal: 'Gorditas', price: 13.00},
+    { meal: 'Chilaquiles Mexicanos', price: 10.00},
+    { meal: 'Flautas', price: 10.00},
+    { meal: 'Super Torta', price: 10.00},
+    { meal: 'Mexican Salad', price: 10.00},
+];
+
+const desserts = [
+    { meal: 'Arroz con Leche', price: 10.00 },
+    { meal: 'Flan', price: 10.00 },
+    { meal: 'Marranitos', price: 10.00 },
+    { meal: 'Buñuelos', price: 10.00},
+    { meal: 'Pastel de Elote', price: 10.00},
+    { meal: 'Tres Leches Cake', price: 10.00},
+    { meal: 'Carlota de Limon', price: 10.00},
+];
+
+const drinks = [
+    { meal: 'Tequila', price: 12.00 },
+    { meal: 'Refrescos', price: 7.00 },
+    { meal: 'Margaritas', price: 15.00 },
+    { meal: 'Ponche de Frutas', price: 11.00},
+    { meal: 'Michelada', price: 9.00},
+];
+
+export { menuTacos, quesadillas, others, desserts, drinks };
